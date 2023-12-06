96b86d14c38901a21a91412c7e89a8ef6b746e65
imarati <iimarioii3@mail.ru>
2023-12-06 18:19:00 +0300

    var

--- Homework11/Hw11/Services/ExpressionVisitorDispatcher/ExpressionVisitorDispatcher.cs
+++ Homework11/Hw11/Services/ExpressionVisitorDispatcher/ExpressionVisitorDispatcher.cs
@@ -22,4 +22,10 @@ public class ExpressionVisitorDispatcher
         var exp = new ParallelEvaluationVisitor().VisitConstant(expression);
         return Expression.Lambda<Func<double>>(exp).Compile().Invoke();
     }
+    
+    public static async Task<double> Visit(Expression expression)
+    {
+        dynamic exp = expression;
+        return Visit(exp);
+    }
 }
