5fcfd077bde71d0c3480a888bdc1b0e518b59be9
imarati <iimarioii3@mail.ru>
2023-12-06 15:52:18 +0300

    unary

--- Homework11/Hw11/Services/MathCalculator/ParallelEvaluationVisitor.cs
+++ Homework11/Hw11/Services/MathCalculator/ParallelEvaluationVisitor.cs
@@ -12,6 +12,10 @@ public class ParallelEvaluationVisitor
             await Task.Delay(1000);
             if (node.Left is BinaryExpression binaryLeft)
                 return await VisitBinaryAsync(binaryLeft);
+            if (node.Left is UnaryExpression unaryLeft)
+                return await VisitUnaryAsync(unaryLeft);
+            if (node.Left is ConstantExpression constantLeft)
+                return VisitConstant(constantLeft);
             return node.Left;
         });
         var secondTask = new Lazy<Task<Expression>>(async () =>
@@ -19,6 +23,10 @@ public class ParallelEvaluationVisitor
             await Task.Delay(1000);
             if (node.Right is BinaryExpression binaryLeft)
                 return await VisitBinaryAsync(binaryLeft);
+            if (node.Right is UnaryExpression unaryRight)
+                return await VisitUnaryAsync(unaryRight);
+            if (node.Right is ConstantExpression constantRight)
+                return VisitConstant(constantRight);
             return node.Right;
         });
 
@@ -30,6 +38,37 @@ public class ParallelEvaluationVisitor
                 throw new DivideByZeroException(MathErrorMessager.DivisionByZero);
         }
 
+        return node.NodeType switch
+        {
+            ExpressionType.Add => Expression.Add(result[0], result[1]),
+            ExpressionType.Subtract => Expression.Subtract(result[0], result[1]),
+            ExpressionType.Multiply => Expression.Multiply(result[0], result[1]),
+            _ => Expression.Divide(result[0], result[1])
+        };
+    }
+
+    public async Task<Expression> VisitUnaryAsync(UnaryExpression node)
+    {
+        var operandTask = new Lazy<Task<Expression>>(async () =>
+        {
+            await Task.Delay(1000);
+            if (node.Operand is BinaryExpression binaryOperand)
+                return await VisitBinaryAsync(binaryOperand);
+            if (node.Operand is UnaryExpression unaryOperand)
+                return await VisitUnaryAsync(unaryOperand);
+            if (node.Operand is ConstantExpression constantOperand)
+                return VisitConstant(constantOperand);
+            return node.Operand;
+        });
+
+        var result = await Task.WhenAll(operandTask.Value);
+
+        if (node.NodeType == ExpressionType.Divide)
+        {
+            if (Expression.Lambda<Func<double>>(result[1]).Compile().Invoke() == 0)
+                throw new DivideByZeroException(MathErrorMessager.DivisionByZero);
+        }
+        
         return node.NodeType switch
         {
             ExpressionType.Add => Expression.Add(result[0], result[1]),
