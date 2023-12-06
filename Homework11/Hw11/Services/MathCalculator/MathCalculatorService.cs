7ec43ed8fa935b264b35df63183919e5816480fa
imarati <iimarioii3@mail.ru>
2023-12-06 18:21:25 +0300

    adfskj;l

--- Homework11/Hw11/Services/MathCalculator/MathCalculatorService.cs
+++ Homework11/Hw11/Services/MathCalculator/MathCalculatorService.cs
@@ -7,7 +7,7 @@ public class MathCalculatorService : IMathCalculatorService
     public async Task<double> CalculateMathExpressionAsync(string? expression)
     {
         MathExpressionValidatorService.ValidateExpression(expression);
-        var tree = MathParserService.ParseExpression(expression!);
+        dynamic tree = MathParserService.ParseExpression(expression!);
         var result = await ExpressionVisitorDispatcher.ExpressionVisitorDispatcher.Visit(tree);
 
         return result;
