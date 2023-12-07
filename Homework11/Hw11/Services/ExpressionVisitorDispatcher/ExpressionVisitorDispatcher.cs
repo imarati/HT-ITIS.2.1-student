50e4e9341c63b4afba78be739e24e670241dde2a
imarati <iimarioii3@mail.ru>
2023-12-07 23:11:15 +0300

    ???

--- Homework11/Hw11/Services/ExpressionVisitorDispatcher/ExpressionVisitorDispatcher.cs
+++ Homework11/Hw11/Services/ExpressionVisitorDispatcher/ExpressionVisitorDispatcher.cs
@@ -7,19 +7,19 @@ public class ExpressionVisitorDispatcher
 {
     public static async Task<double> Visit(BinaryExpression expression)
     {
-        var exp = await new ParallelEvaluationVisitor().VisitBinaryAsync(expression);
+        var exp = await new ParallelEvaluationVisitor().VisitBinaryAsync((dynamic)expression);
         return Expression.Lambda<Func<double>>(exp).Compile().Invoke();
     }
     
     public static async Task<double> Visit(UnaryExpression expression)
     {
-        var exp = await new ParallelEvaluationVisitor().VisitUnaryAsync(expression);
+        var exp = await new ParallelEvaluationVisitor().VisitUnaryAsync((dynamic)expression);
         return Expression.Lambda<Func<double>>(exp).Compile().Invoke();
     }
 
     public static async Task<double> Visit(ConstantExpression expression)
     {
-        var exp = new ParallelEvaluationVisitor().VisitConstant(expression);
+        var exp = new ParallelEvaluationVisitor().VisitConstant((dynamic)expression);
         return Expression.Lambda<Func<double>>(exp).Compile().Invoke();
     }
 }
