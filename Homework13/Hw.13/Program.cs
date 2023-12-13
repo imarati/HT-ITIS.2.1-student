164db181039dfb810aaa9f16d0817e74130aad88
imarati <iimarioii3@mail.ru>
2023-12-13 20:55:18 +0300

    final

--- Homework13/Hw.13/Program.cs
+++ Homework13/Hw.13/Program.cs
@@ -1,5 +1,15 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
 using Hw13;
 
-BenchmarkRunner.Run<MemoryTests>();
+var config = new ManualConfig()
+    .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+    .AddValidator(JitOptimizationsValidator.DontFailOnError)
+    .AddLogger(ConsoleLogger.Default)
+    .AddColumnProvider(DefaultColumnProviders.Instance);
+
+BenchmarkRunner.Run<MemoryTests>(config);
    
